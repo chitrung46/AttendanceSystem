5eee40479ed1d3b1c302e0535b76855595adc4b8
Nhat Toan <nhattoan664t@gmail.com>
2024-09-27 22:35:30 +0700

    test

--- AttendanceSystem/Program.cs
+++ AttendanceSystem/Program.cs
@@ -17,7 +17,8 @@ namespace AttendanceSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            //Application.Run(new LoginForm());
+            Application.Run(new StudentForm());
         }
     }
 }
