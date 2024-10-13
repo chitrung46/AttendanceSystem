29bf3891d428b582c6db588e7dba82bbd1ee5342
Luong Chi Trung <luongchitrung53@gmail.com>
2024-10-14 02:36:08 +0700

    complete Login function

---
+++ DAL/AccountAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public class AccountAccess:DatabaseAccess
+    {
+        public string checkLogin(Account acc)
+        {
+            string info = checkLoginDTO(acc);
+            return info;
+        }
+    }
+}
---
+++ DAL/DatabaseAccess.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public class SqlConnectionData
+    {
+        public static SqlConnection Connect()
+        {
+            string strcon = @"Data Source=CHITRUNG-LAPTOP\TRUNG_SQL;Initial Catalog=ATTENDANCE_SYSTEM;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strcon);
+            return conn;
+        }
+    }
+    
+    public class DatabaseAccess
+    {
+        public static string checkLoginDTO(Account acc)
+        {
+            string user = null;
+            SqlConnection conn = SqlConnectionData.Connect();
+            conn.Open();
+            SqlCommand command = new SqlCommand("proc_login", conn);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@email", acc.email);
+            command.Parameters.AddWithValue("@pass", acc.sPassword);
+
+            command.Connection = conn;
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    user = reader.GetString(0);
+                }
+                reader.Close();
+                conn.Close();
+            }
+            else
+            {
+                return "Email hoặc mật khẩu không chính xác!";
+            }
+
+            return user;
+        }
+    }
+}
